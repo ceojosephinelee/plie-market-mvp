cebb090421b982a7cfd5388b64f989a74cc9f4d0
Jihyun <jose1123@naver.com>
2025-09-18 19:55:22 +0900

    init: start Spring Boot project

---
+++ src/main/java/com/example/plie_market_mvp/PlieMarketMvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.plie_market_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlieMarketMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlieMarketMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plie_market_mvp/PlieMarketMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plie_market_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlieMarketMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
